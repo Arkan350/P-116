922d83f79f52a527e6baca908c4fdf301466b165
Arkan350 <113187889+Arkan350@users.noreply.github.com>
2023-01-08 12:02:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+var nose_x = 0;
+var nose_y = 0;
+
+function preload() {
+    nose1 = loadImage('m.png')
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO)
+    video.size(300, 300)
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    image(nose1,nose_x,nose_y,80,40)
+}
+
+function take_snapshot() {
+    save("arkan.png");
+}
+
+function modelLoaded() {
+    console.log("Model is successfully loaded");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        nose_x = results[0].pose.nose.x-40;
+        nose_y = results[0].pose.nose.y;
+    }
+
+}
